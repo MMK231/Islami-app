280d4fad7c3d34d8260dcc859cb5a308e2e2820c
Mo Khaled <mmk.official23@gmail.com>
2025-07-19 02:57:08 +0300

    feat:Hadith details

--- app/src/main/java/com/course/islamiapp/Home/Hadith/HadithFragment.kt
+++ app/src/main/java/com/course/islamiapp/Home/Hadith/HadithFragment.kt
@@ -11,7 +11,6 @@ import com.course.islamiapp.databinding.FragmentHadithBinding
 
 class HadithFragment : Fragment() {
     lateinit var binding: FragmentHadithBinding
-    lateinit var hadeth: Hadeth
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
